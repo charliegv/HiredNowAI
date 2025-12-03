8ff4b1fa291e9a983f54367628ec81d002c3a4c8
charliegv <charlie_vinall@yahoo.co.uk>
2025-12-03 22:01:38 +0000

    Added sys path on worker.py

--- workers/worker.py
+++ workers/worker.py
@@ -1,12 +1,17 @@
 # ---- CLEAN + CORRECTED WORKER ----
-
-import asyncio
-import asyncpg
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import logging
 import json
 import aiohttp
 import uuid
+
+import asyncio
+import asyncpg
+
 
 # from bots.greenhouse import GreenhouseBot
 from bots.lever import LeverBot
